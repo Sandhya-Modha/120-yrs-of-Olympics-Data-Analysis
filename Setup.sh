c2e079139b269518a1f864fe102356becb722164
Sandhya-Modha <sandhyamodha1011@gmail.com>
2025-05-09 09:09:19 +0530

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
-
-# Upgrade pip to avoid build issues
-pip install --upgrade pip
-
-# Install build tools
-pip install setuptools wheel
-
-# Install the required dependencies from requirements.txt
-pip install -r requirements.txt
-
-# Streamlit configuration setup
 mkdir -p ~/.streamlit/
 
 echo "\
 [server]\n\
-port = $PORT\n\
+port = \$PORT\n\
 enableCORS = false\n\
 headless = true\n\
-\n\
 " > ~/.streamlit/config.toml
+
+pip install --upgrade pip
+pip install -r requirements.txt
